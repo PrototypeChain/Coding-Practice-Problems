cd97dd3887b839ff1085bbb3ab71194670b52ac6
heyvivek31@gmail.com <heyvivek31@gmail.com>
2019-08-15 11:30:34 +0530

    LIS Recursion Added

--- src/main/java/com/practice/strings/LongestIncreasingSubsequence.java
+++ src/main/java/com/practice/strings/LongestIncreasingSubsequence.java
@@ -4,7 +4,24 @@ import java.util.Arrays;
 
 public class LongestIncreasingSubsequence {
 
+    //Recursive Solution - LeetCode - Length of LIS
+    public int lengthOfLIS(int[] nums) {
+        return lengthofLIS(nums, Integer.MIN_VALUE, 0);
+    }
 
+    public int lengthofLIS(int[] nums, int prev, int curpos) {
+        if (curpos == nums.length) {
+            return 0;
+        }
+        int taken = 0;
+        if (nums[curpos] > prev) {
+            taken = 1 + lengthofLIS(nums, nums[curpos], curpos + 1);
+        }
+        int nottaken = lengthofLIS(nums, prev, curpos + 1);
+        return Math.max(taken, nottaken);
+    }
+
+    //DP Solution
     public static int getLis(int[] arr){
         int[] LIS = new int[arr.length];
         for ( int i = 0; i < arr.length; i++ ) {
@@ -41,8 +58,12 @@ public class LongestIncreasingSubsequence {
     }
 
     public static void main(String[] args) {
+        LongestIncreasingSubsequence lis  = new LongestIncreasingSubsequence();
         int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+
         int len = getLis(arr);
         System.out.println(len);
+
+        System.out.println("Recursion: " + lis.lengthOfLIS(arr));
     }
 }
