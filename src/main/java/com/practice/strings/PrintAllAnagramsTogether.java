93bd445fd47814a16d2854cfcf5441353ba20f32
heyvivek31@gmail.com <heyvivek31@gmail.com>
2019-08-15 23:00:17 +0530

    Anagram

--- src/main/java/com/practice/strings/PrintAllAnagramsTogether.java
+++ src/main/java/com/practice/strings/PrintAllAnagramsTogether.java
@@ -1,7 +1,6 @@
 package com.practice.strings;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrintAllAnagramsTogether {
 
@@ -33,6 +32,59 @@ public class PrintAllAnagramsTogether {
                 System.out.println();
             }
         }
+    }
+
+
+    //Approach 1: Categorize by Sorted String
+    /*Intuition
+        Two strings are anagrams if and only if their sorted strings are equal.
+        Algorithm
+        Maintain a map ans : {String -> List} where each key \text{K}K is a sorted string, and each value is the list of strings from the initial input that when sorted, are equal to \text{K}K.
+
+        In Java, we will store the key as a string, eg. code. In Python, we will store the key as a hashable tuple, eg. ('c', 'o', 'd', 'e').
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+
+
+    //**Approach 2: Categorize by Count
+    //Intuition
+    //
+    //Two strings are anagrams if and only if their character counts (respective number of occurrences of each character) are the same.
+    //
+    //Algorithm
+    //
+    //We can transform each string \text{s}s into a character count, \text{count}count, consisting of 26 non-negative integers representing the number of \text{a}a's, \text{b}b's, \text{c}c's, etc. We use these counts as the basis for our hash map.
+    //
+    //In Java, the hashable representation of our count will be a string delimited with '#' characters. For example, abbccc will be #1#2#3#0#0#0...#0 where there are 26 entries total. In python, the representation will be a tuple of the counts. For example, abbccc will be (1, 2, 3, 0, 0, ..., 0), where again there are 26 entries total.
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
     }
 
     public static void main(String[] args) {
